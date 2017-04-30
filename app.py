164d12951eab85d2866c4358be7f4eabb0737e3b
Collin James <mail@collinjam.es>
2017-04-30 17:07:37 -0400

    mostly working

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ class Boat(ndb.Model):
 class Slip(ndb.Model):
 	sid = ndb.StringProperty() #use a key?
 	number = ndb.IntegerProperty(required=True)
-	current_boat = ndb.StringProperty(required=True)
-	arrival_date = ndb.StringProperty(required=True)
-	departure_history = ndb.IntegerProperty(required=True)
+	current_boat = ndb.StringProperty()
+	arrival_date = ndb.StringProperty()
+	# departure_history = ndb.IntegerProperty() # extra credit
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
@@ -45,9 +45,21 @@ class BoatHandler(BaseHandler):
 	 
 	def post(self):
 		'''handle POST requests (add new items)'''
-		# parent_key = ndb.Key(Fish, "parent_fish")
 		boat_data = json.loads(self.request.body)
-		new_boat = Boat(name=boat_data['name'], btype=boat_data['btype'], blength=boat_data['blength'], at_sea=False)
+		if not "name" in boat_data:
+			# error
+			self.handleClientError(400, 'Please provide a name')
+			return
+		if not "btype" in boat_data:
+			# error
+			self.handleClientError(400, 'Please provide a boat type')
+			return
+		if not "blength" in boat_data:
+			# error
+			self.handleClientError(400, 'Please provide a boat length')
+			return
+
+		new_boat = Boat(name=boat_data['name'], btype=boat_data['btype'], blength=boat_data['blength'], at_sea=True)
 		new_boat.put()
 		new_boat.bid = str(new_boat.key.id())
 		new_boat.put()
@@ -55,27 +67,29 @@ class BoatHandler(BaseHandler):
 		boat_dict['self'] = '/boat/' + new_boat.key.urlsafe()
 		self.response.write(json.dumps(boat_dict))
 
-	def get(self, id=None):
-		'''handle GET requests (get information about an item)'''
-		if id:
-			b = ndb.Key(urlsafe=id).get()
-			b_d = b.to_dict()
-			b_d['self'] = "/boat/" + id
-			self.response.write(json.dumps(b_d))
+	def get(self, url=None):
+		'''handle GET requests (get information about an item or all items)'''
+		# handle all items
+		if "/boats" in url:
+			boats = []
+			boats_qry = Boat.query()
+			all_boats = boats_qry.fetch()
+			for boat in all_boats:
+				b = boat.to_dict()
+				b['self'] = '/boat/' + boat.key.urlsafe() # get key for url
+				boats.append(b)
+				
+			self.response.write(json.dumps(boats))
+		# handle one item
 		else:
-			self.handleClientError(400, 'Please provide a valid id')
-
-	def get(self):
-		'''handle GET requests (get information about ALL items)'''
-		boats = []
-		boats_qry = Boat.query()
-		all_boats = boats_qry.fetch()
-		for boat in all_boats:
-			b = boat.to_dict()
-			b['self'] = '/boat/' + boat.key.urlsafe() # get key for url
-			boats.append(b)
-			
-		self.response.write(json.dumps(boats))
+			id = url
+			if id:
+				b = ndb.Key(urlsafe=id).get()
+				b_d = b.to_dict()
+				b_d['self'] = "/boat/" + id
+				self.response.write(json.dumps(b_d))
+			else:
+				self.handleClientError(400, 'Please provide an id')
 		
 	def patch(self, id=None):
 		'''handle patch requests (modify info about an item)'''
@@ -144,8 +158,16 @@ class BoatHandler(BaseHandler):
 		'''hand delete requests (delete a boat)'''
 		if id:
 			b = ndb.Key(urlsafe=id).get()
-			if(b):
+			if(b != None):
 				# remove the boat from the slip
+				if b.at_sea == False:
+					# search for the slip
+					# slip_qry = ndb.gql(("SELECT * from Slip WHERE current_boat = \' "+"/boat/"+id+"\'"))
+					slip_qry = ndb.gql("SELECT * from Slip WHERE current_boat = :1", str("/boat/"+id))
+					slip = slip_qry.fetch()[0]
+					print slip
+					slip.current_boat = None
+					slip.put()
 				# delete the entity
 				k = b.key
 				k.delete() # delete is called on a key
@@ -157,12 +179,41 @@ class BoatHandler(BaseHandler):
 			self.handleClientError(400, 'Please provide a valid id')
 
 class SlipHandler(BaseHandler):
-	 
-	def post(self):
-		'''handle POST requests (add new items)'''
+
+	def findNumber(self, number):
+		# # look for slips with the supplied number and ask for a different one
+		slips_qry = Slip.query(Slip.number == number)
+		all_slips = slips_qry.fetch()
+		if len(all_slips):
+			slips = []
+			slips_qry = ndb.gql("SELECT number FROM Slip")
+			all_slips = slips_qry.fetch()
+		
+			for slip in all_slips:
+				s = slip.to_dict()
+				slips.append(s)
+			self.handleClientError(403, '')
+			self.response.write(json.dumps(slips))
+			return 1
+
+		return 0
+
+	def post(self, id=None):
+		'''handle POST requests (add new item)'''
 		# parent_key = ndb.Key(Fish, "parent_fish")
 		slip_data = json.loads(self.request.body)
-		new_slip = Boat(name=slip_data['name'], stype=slip_data['stype'], slength=slip_data['slength'], at_sea=False)
+		if not "number" in slip_data:
+			# error
+			self.handleClientError(400, 'Please provide a slip number')
+			return
+
+		# # look for slips with the supplied number and ask for a different one
+		end = self.findNumber(slip_data["number"])
+		if end:
+			return
+
+		# valid request
+		new_slip = Slip(number=slip_data["number"])
 		new_slip.put()
 		new_slip.sid = str(new_slip.key.id())
 		new_slip.put()
@@ -170,33 +221,59 @@ class SlipHandler(BaseHandler):
 		slip_dict['self'] = '/slip/' + new_slip.key.urlsafe()
 		self.response.write(json.dumps(slip_dict))
 
-	def get(self, id=None):
-		'''handle GET requests (get information about an item)'''
-		if id:
-			s = ndb.Key(urlsafe=id).get()
-			s_d = s.to_dict()
-			s_d['self'] = "/slip/" + id
-			self.response.write(json.dumps(s_d))
-		else:
-			self.handleClientError(400, 'Please provide a valid id')
+	def get(self, id=None): #[DONE]
+		'''handle GET requests (get information about an item or all items)'''
+		path = self.request.path
+		# handle boat docking [DONE]
+		if "/boat" in path:
+			parts = id.split('/boat/')
+			id = parts[0]
+			if id:
+				s = ndb.Key(urlsafe=id).get()
+				if s != None:
+					s_d = s.to_dict()
+					print s_d
+					if testNull(s_d["current_boat"]):
+						# return boat information
+						BoatHandler(response=self.response).get(s_d["current_boat"].split('/boat/')[1])
+						return
+					else:
+						self.handleClientError(204, 'No boat')
+						return
+				else:
+					self.handleClientError(403, 'No such slip')
+					return
 
-	def get(self):
-		'''handle GET requests (get information about ALL items)'''
-		slips = []
-		slips_qry = Boat.query()
-		all_slips = slips_qry.fetch()
-		for slip in all_slips:
-			s = slip.to_dict()
-			s['self'] = '/slip/' + slip.key.urlsafe() # get key for url
-			slips.append(s)
-			
-		self.response.write(json.dumps(slips))
-		
-	def patch(self, id=None):
+		# handle all items [DONE]
+		elif "/slips" in path:
+			slips = []
+			slips_qry = Slip.query()
+			all_slips = slips_qry.fetch()
+			for slip in all_slips:
+				s = slip.to_dict()
+				s['self'] = '/slip/' + slip.key.urlsafe() # get key for url
+				slips.append(s)
+				
+			self.response.write(json.dumps(slips))
+		# handle one item [DONE]
+		else:
+			if id:
+				s = ndb.Key(urlsafe=id).get()
+				if s != None:
+					s_d = s.to_dict()
+					s_d['self'] = "/slip/" + id
+					self.response.write(json.dumps(s_d))
+				else:
+					self.handleClientError(204, 'No such slip')
+					return
+			else:
+				self.handleClientError(400, 'Please provide a valid id')
+
+	def patch(self, id=None): #[FINISH ME]
 		'''handle patch requests (modify info about an item)'''
 		if id:
 			s = ndb.Key(urlsafe=id).get()
-			if(s):
+			if(s != None):
 				s_d = s.to_dict()
 				patch_data = json.loads(self.request.body)
 			else:
@@ -206,15 +283,19 @@ class SlipHandler(BaseHandler):
 			# succeeded, parse patch_data
 			for key in patch_data:
 				if(key in s_d):
-					if(str(key) == 'at_sea'):
-						self.handleClientError(403, 'Please use slip API to manage at_sea')
+					if(str(key) == 'current_boat'):
+						self.handleClientError(403, 'Please use PUT /slip/id/boat')
 						return
 					if(str(key) == 'sid'):
 						self.handleClientError(403, 'Attribute cannot be set')
 						return
+					if(str(key) == 'number'):
+						if str(key) != s_d["number"]:
+							end = self.findNumber(patch_data[key])
+							if end:
+								return
 
 					setattr(b, str(key), patch_data[key]) #works!
-					##! FIXME - don't allow at_sea to be set here
 				else:
 					self.handleClientError(400, 'Key \''+str(key)+'\' not valid')
 					return # we're done here :(
@@ -226,50 +307,137 @@ class SlipHandler(BaseHandler):
 		else:
 			self.handleClientError(400, 'Please provide a valid id')
 
-	def put(self, id=None):
-		'''handle put requests (replace an item)'''
-		if id:
-			s = ndb.Key(urlsafe=id).get()
-			if(s):
-				s_d = s.to_dict()
-				required = [u"name", u"stype", u"slength"]
-				put_data = json.loads(self.request.body)
-			else:
-				self.handleClientError(204, 'No such slip')
+	def put(self, url=None):
+		'''handle put requests (replace an item, add a boat)'''
+		# handle boat docking [DONE]
+		if "/boat/" in url:
+			# split up path to bet bid
+			parts = url.split('/boat/')
+			id = parts[0]
+			put_data = json.loads(self.request.body)
+			if not "arrival_date" in put_data:
+				self.handleClientError(400, 'Please provide an arrival date')
 				return
-
-			if( set(required) == set(put_data.keys()) ): #all required data is present
-				# process the data
-				for key in put_data:
-					setattr(b, str(key), put_data[key])
+			if "bid" in put_data: # got a boat id
+				bid = put_data["bid"]
+				b = ndb.Key(urlsafe=bid).get()
+				if id:
+					s = ndb.Key(urlsafe=id).get()
+					s_d = s.to_dict()
+					if testNull(s_d["current_boat"]):
+						self.handleClientError(403, 'A boat already exists')
+						# self.response.write(json.dumps(s_d["current_boat"]))
+						return
+					else:
+						# add boat to slip
+						s.current_boat = "/boat/" + bid
+						s.arrival_date = put_data["arrival_date"]
+						s.put()
+						# update boat to be not at sea
+						b.at_sea = False
+						b.put()
+					s_d = s.to_dict()
+					s_d['self'] = "/slip/" + id
+					self.response.write(json.dumps(s_d))
+				else:
+					self.handleClientError(400, 'Please provide a slip id')
+					return
 			else:
-				# sent an error
-				self.handleClientError(400, 'Please supply all required keys')
+				self.handleClientError(400, 'Please provide a boat id')
 				return
-			# success! write to the db
-			s.put()
-			s_d = s.to_dict()
-			s_d['self'] = "/slip/" + id
-			self.response.write(json.dumps(s_d))
-
+		# handle replace data (don't allow setting a boat) [FINISH ME]
 		else:
-			self.handleClientError(400, 'Please provide a valid id')
+			id = url
+			if id:
+				s = ndb.Key(urlsafe=id).get()
+				if(s):
+					s_d = s.to_dict()
+					required = [u"number"]
+					put_data = json.loads(self.request.body)
+				else:
+					self.handleClientError(204, 'No such slip')
+					return
 
-	def delete(self, id=None):
+				# if( set(required) & set(put_data.keys()) ): #all required data is present
+				if "number" in put_data: #all required data is present
+					if put_data["number"] != s.number:
+						# look for slips with the supplied number and ask for a different one
+						end = self.findNumber(put_data["number"])
+						if end:
+							return
+					# process the data
+					if "bid" in put_data:
+						# check for boat in current slip and set it to at sea
+						if not "arrival_date" in put_data:
+							self.handleClientError(400, 'Currently need arrival date for replace')
+							return
+
+						if testNull(s.current_boat):
+							# update boat to be at sea
+							b = ndb.Key(urlsafe=s_d["current_boat"].split('/boat/')[1]).get()
+							if b:
+								b.at_sea = True
+							else:
+								self.handleClientError(204, 'No such boat')
+								return
+							# b.put()
+						# add boat
+						s.current_boat = "/boat/" + put_data["bid"]
+						s.arrival_date = put_data["arrival_date"]
+						b2 = ndb.Key(urlsafe=put_data["bid"])
+						b2.at_sea = False
+						b.put()
+						b2.put()
+
+					elif "arrival_date" in put_data:
+						s.arrival_date = put_data["arrival_date"]
+
+					# set new number
+					s.number = put_data["number"]
+					# write all data
+					s.put()
+					s_d = s.to_dict()
+					s_d['self'] = "/slip/" + id
+					self.response.write(json.dumps(s_d))
+				else:
+					# send an error
+					self.handleClientError(400, 'Please supply only required keys')
+					return
+			else:
+				self.handleClientError(400, 'Please provide a valid id')
+
+	def delete(self, url=None):
 		'''hand delete requests (delete a slip)'''
-		if id:
-			s = ndb.Key(urlsafe=id).get()
-			if(s):
-				# remove the slip from the slip
-				# delete the entity
-				k = s.key
-				k.delete() # delete is called on a key
-				self.handleClientError(204, '') # not actually an error, but we have this nice function :)
-			else:
-				self.handleClientError(404, 'No such slip')
-				return
+		if "/boat" in url:
+			pass
+		# handle slip deletion
 		else:
-			self.handleClientError(400, 'Please provide a valid id')
+			id = url
+			if id:
+				s = ndb.Key(urlsafe=id).get()
+				s_d = s.to_dict()
+				if(s):
+					# remove the boat from the slip
+					print "HEYYYYYYYY"
+					print  s_d["current_boat"]
+					if testNull(s_d["current_boat"]):
+						b = ndb.Key(urlsafe=s_d["current_boat"].split('/boat/')[1]).get()
+						b.at_sea = True
+						b.put()
+					# delete the entity
+					k = s.key
+					k.delete() # delete is called on a key
+					
+					self.handleClientError(204, '') # not actually an error, but we have this nice function :)
+					return				
+				else:
+					self.handleClientError(404, 'No such slip')
+					return
+			else:
+				self.handleClientError(400, 'Please provide a valid id')
+
+def testNull(term):
+	return (term != "null" and term != None)
 
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH', ))
@@ -277,7 +445,12 @@ webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 
 app = webapp2.WSGIApplication([
 	('/', MainPage),
-	('/boat', BoatHandler), # create a new boat
+	('/boat', BoatHandler), # "post" (create) a new boat
 	('/boats', BoatHandler), # return all boats
-	('/boat/(.*)', BoatHandler), # get, post, patch, or delete a specific boat (pass id) 
+	('/boat/(.*)', BoatHandler), # get, put, patch, or delete a specific boat (/boat/id) 
+	('/slip', SlipHandler), # "post" (create) a new slip
+	('/slips', SlipHandler), # return all slips
+	('/slip/(.*)', SlipHandler), # get, put, patch, or delete a specific slip (/slip/id)
+	('/slip/(.*)/boat', SlipHandler), # "get", "delete" or "put" a boat at slip (/slip/id/boat)
+	# ('/slip/(.*)/boat/(.*)', SlipHandler), # "put" a boat in slip (/slip/id/boat/id)
 	], debug=True)
